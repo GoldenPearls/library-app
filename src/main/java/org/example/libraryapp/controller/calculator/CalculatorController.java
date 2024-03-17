94c61871d173504f282c494dd63d8247a0069866
Geυмjυ lee <97003348+GoldenPearls@users.noreply.github.com>
2024-03-17 17:29:52 +0900

    1. post 형식 구현 2. 곱셈

--- src/main/java/org/example/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/org/example/libraryapp/controller/calculator/CalculatorController.java
@@ -1,7 +1,10 @@
 package org.example.libraryapp.controller.calculator;
 
 import org.example.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import org.example.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // api 진입 지점으로 생각하겠다. api는 controller로 들어오고, controller는 service로 들어간다.
@@ -10,5 +13,10 @@ public class CalculatorController {
     public int addTwoNumbers(
             CalculatorAddRequest request) {// CalculatorAddRequest 객체를 받아서 request라는 이름으로 사용하겠다.
         return request.getNumber1() + request.getNumber2(); // 받은 request 객체의 number1과 number2를 더해서 반환
+    }
+
+    @PostMapping("/multiply") // HTTP Method가 post 방식으로 http path가 /multiply인 API로 지정
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {// CalculatorMultiplyRequest 객체를 받아서 request라는 이름으로 사용하겠다.
+        return request.getNumber1() * request.getNumber2(); // 받은 request 객체의 number1과 number2를 곱해서 반환
     }
 }
